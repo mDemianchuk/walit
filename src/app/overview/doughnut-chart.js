d1efb0d57fefaf71dc6dfe820af94222296461a5
Mykola Demianchuk <nicksetone@gmail.com>
2019-02-18 13:50:15 -0600

    feat: add website skeleton

---
+++ src/app/overview/doughnut-chart.js
@@ -0,0 +1,21 @@
+new Chart(document.getElementById("doughnut-chart"), {
+    type: 'doughnut',
+    data: {
+
+        labels: ["Rent", "Tuition", "Bills", "Gas", "Insurance"],
+        datasets: [
+            {
+                label: "$",
+                backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                data: [600,1000,234,112,75]
+            }
+        ]
+    },
+    options: {
+        responsive: false,
+        title: {
+            display: true,
+            text: 'Where did you spend your money?'
+        }
+    }
+});
