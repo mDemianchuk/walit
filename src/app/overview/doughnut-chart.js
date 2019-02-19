22715bab96a097c4db2252f146b89d03740780e3
Mykola Demianchuk <nicksetone@gmail.com>
2019-02-19 11:51:56 -0600

    refactor: move the logic to methods

--- src/app/overview/doughnut-chart.js
+++ src/app/overview/doughnut-chart.js
@@ -1,12 +1,12 @@
-new Chart(document.getElementById("doughnut-chart"), {
+new Chart(document.getElementById('doughnut-chart'), {
     type: 'doughnut',
     data: {
 
-        labels: ["Rent", "Tuition", "Bills", "Gas", "Insurance"],
+        labels: ['Rent', 'Tuition', 'Bills', 'Gas', 'Insurance'],
         datasets: [
             {
-                label: "$",
-                backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                label: '$',
+                backgroundColor: ['#3e95cd', '#8e5ea2','#3cba9f','#e8c3b9','#c45850'],
                 data: [600,1000,234,112,75]
             }
         ]
