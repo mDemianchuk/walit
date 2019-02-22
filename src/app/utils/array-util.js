b8537db5660899c8383c4655a729f94e21179c21
Shend Vishi <vishi.shend@gmail.com>
2019-02-22 09:47:42 -0600

    fix: import/export error

---
+++ src/app/utils/array-util.js
@@ -0,0 +1,17 @@
+export function incrementFill(start, end) {
+    let arr = new Array(end);
+    for (let i = start; i <= end; i++) {
+        arr[i - 1] = i;
+    }
+    return arr;
+}
+
+export function randomFill(len, step) {
+    let arr = new Array(len);
+    let sum = 0;
+    for (let i = 1; i <= len; i++) {
+        sum += i * (Math.floor(Math.random() * step) + 1);
+        arr[i - 1] = sum;
+    }
+    return arr;
+}
