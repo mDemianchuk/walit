c1bde062ad0ff815ecdc507b3016b424511811db
Mykola Demianchuk <nicksetone@gmail.com>
2019-03-18 01:12:29 -0500

    feat: add transaction model

--- assets/scripts/utils/date-util.js
+++ assets/scripts/utils/date-util.js
@@ -7,7 +7,19 @@ function getNumberOfDaysInPreviousMonth() {
     return new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 0).getDate();
 }
 
+function getShortDate(date) {
+    let month = toTwoDigit(date.getMonth() + 1);
+    let day = toTwoDigit(date.getDate() + 1);
+    let year = date.getFullYear();
+    return `${month}/${day}/${year}`;
+
+    function toTwoDigit(digit) {
+        return (digit < 10) ? `0${digit}` : digit;
+    }
+}
+
 module.exports = {
     getNumberOfDaysInCurrentMonth,
-    getNumberOfDaysInPreviousMonth
+    getNumberOfDaysInPreviousMonth,
+    getShortDate
 };
---
+++ assets/scripts/utils/json-util.js
@@ -0,0 +1,11 @@
+function jsonToList(jsonString) {
+    let transactionList = JSON.parse(jsonString);
+    for(let transaction of transactionList) {
+        transaction.date = new Date(transaction.date);
+    }
+    return transactionList;
+}
+
+module.exports = {
+    jsonToList
+};
