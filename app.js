eb692751902103bd69db69f993ee9e86d3b95888
vihar25 <vdoshi1@luc.edu>
2019-04-21 18:10:26 -0500

    feat: limit and currency settings

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
 const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
@@ -12,50 +11,54 @@ const mongo = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 
 app.route('/')
-	.get((req, res) => res.redirect('/login'))
-	.post((req, res) => res.redirect('/login'));
+    .get((req, res) => res.redirect('/login'))
+    .post((req, res) => res.redirect('/login'));
 
 app.route('/login')
-	.get((req, res) => res.sendFile(path.join(__dirname + '/public/html/login.html')))
-	.post((req, res) => res.redirect('/overview'));
+    .get((req, res) => res.sendFile(path.join(__dirname + '/public/html/login.html')))
+    .post((req, res) => res.redirect('/overview'));
 
 app.route('/signup')
-	.get((req, res) => res.sendFile(path.join(__dirname + '/public/html/signup.html')))
-	.post((req, res) => res.redirect('/login'));
+    .get((req, res) => res.sendFile(path.join(__dirname + '/public/html/signup.html')))
+    .post((req, res) => res.redirect('/login'));
 
 app.route('/overview')
-	.get((req, res) => res.sendFile(path.join(__dirname + '/public/html/overview.html')));
+    .get((req, res) => res.sendFile(path.join(__dirname + '/public/html/overview.html')));
 
 app.route('/transactions')
-	.get((req, res) => res.sendFile(path.join(__dirname + '/public/html/transactions.html')))
-	.post((req, res) => res.redirect('/transactions'))
+    .get((req, res) => res.sendFile(path.join(__dirname + '/public/html/transactions.html')))
+    .post((req, res) => res.redirect('/transactions'));
 
 app.route('/transactions/update/')
-	.post((req, res) => {
-		console.log(req.body);
-		// res.send("updated " + req.body.edit-description)
-		console.log(req.body.edit_description)
-		//Connect using MongoClient
-		mongo.connect(url,{ useNewUrlParser: true }, function(err, client) {
-			if(err) throw err;
-			let db = client.db('walit');
-			db.collection("transaction").findOne({"uuid": req.params.editId}, function(err, result) {
-				if (err) throw err;
-				if(result == null){
-					console.log("nothing found");
-				}
-				else{
-					console.log(result);
-				}
-				client.close();
-			});
-		});
+    .post((req, res) => {
+        console.log(req.body);
+        // res.send("updated " + req.body.edit-description)
+        console.log(req.body.edit_description);
+        //Connect using MongoClient
+        mongo.connect(url, {useNewUrlParser: true}, function (err, client) {
+            if (err) throw err;
+            let db = client.db('walit');
+            db.collection("transaction").findOne({"uuid": req.params.editId}, function (err, result) {
+                if (err) throw err;
+                if (result == null) {
+                    console.log("nothing found");
+                } else {
+                    console.log(result);
+                }
+                client.close();
+            });
+        });
 
-	});
+    });
 
 app.route('/transactions/delete/:id')
-	.delete((req, res) => res.send("deleted " + req.params.id));
+    .delete((req, res) => res.send("deleted " + req.params.id))
+    .post((req, res) => res.redirect('/transactions'));
+
+app.route('/settings')
+    .get((req, res) => res.sendFile(path.join(__dirname + '/public/html/settings.html')))
+    .post((req, res) => res.redirect('/transactions'));
 
 app.listen(port, () => {
-	console.log(`App listening on port ${port}!`)
+    console.log(`App listening on port ${port}!`)
 });
--- models/line-chart.js
+++ models/line-chart.js
@@ -1,56 +1,60 @@
 const Chart = require('chart.js');
 
-function createChart(element, labels, currentMonthData, previousMonthData, limit) {
-  new Chart(element, {
-    type: 'line',
-    data: {
-      // x axis - days of the month
-      labels: labels,
-      datasets: [{
-        label: 'Spent this month',
-        data: currentMonthData,
-        pointRadius: 1,
-        borderWidth: 1,
-        borderColor: '#000',
-        fill: false,
-      }, {
-        label: 'Spent previous month',
-        data: previousMonthData,
-        pointRadius: 1,
-        borderWidth: 1,
-        borderDash: [3],
-        borderColor: 'rgba(0,0,0,0.3)',
-        fill: false,
-      }, {
-        label: 'Limit',
-        // straight line
-        data: limit,
-        pointRadius: 0,
-        borderWidth: 1,
-        borderDash: [10],
-        borderColor: '#FF0000',
-        backgroundColor: 'rgba(255,0,0,0.1)',
-        fill: 'top',
-      }, ]
-    },
-    options: {
-      legend: {
-        labels: {
-          fontFamily: "'Poppins', sans-serif"
+function createChart(element, labels, currentMonthData, previousMonthData, limit, currency) {
+    new Chart(element, {
+        type: 'line',
+        data: {
+            // x axis - days of the month
+            labels: labels,
+            datasets: [{
+                label: 'Spent this month',
+                data: currentMonthData,
+                pointRadius: 1,
+                borderWidth: 1,
+                borderColor: '#000',
+                fill: false,
+            }, {
+                label: 'Spent previous month',
+                data: previousMonthData,
+                pointRadius: 1,
+                borderWidth: 1,
+                borderDash: [3],
+                borderColor: 'rgba(0,0,0,0.3)',
+                fill: false,
+            }, {
+                label: 'Limit',
+                // straight line
+                data: limit,
+                pointRadius: 0,
+                borderWidth: 1,
+                borderDash: [10],
+                borderColor: '#FF0000',
+                backgroundColor: 'rgba(255,0,0,0.1)',
+                fill: 'top',
+            },]
+        },
+        options: {
+            legend: {
+                labels: {
+                    fontFamily: "'Poppins', sans-serif"
+                }
+            },
+            scales: {
+                xAxes: [{
+                    display: false
+                }],
+                yAxes: [{
+                    ticks: {
+                        callback: function (value, index, values) {
+                            return currency + value;
+                        }
+                    }
+                }]
+            }
         }
-      },
-      scales: {
-        xAxes: [{
-          display: false
-        }],
-        yAxes: [{
-          display: false
-        }]
-      }
-    }
-  });
+    });
 }
 
 module.exports = {
-  createChart
+    createChart
 };
