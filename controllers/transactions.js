4948dcee393081ffbbc158338f52fcd0cdc05db5
mdemianchuk <nickdemianchuk@gmail.com>
2019-04-27 16:55:18 -0500

    refactor: find -> filter transactions

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -8,14 +8,14 @@ const transactionContainer = document.getElementById('transactions-container');
 const transactionTableBody = document.getElementById('transactions-table-body');
 
 const addTransactionButton = document.getElementById('add-transaction-button');
-const findTransactionsButton = document.getElementById('find-transactions-button');
+const filterTransactionsButton = document.getElementById('filter-transactions-button');
 
 const addTransactionModal = document.getElementById('add-transaction-modal');
-const findTransactionsModal = document.getElementById('find-transactions-modal');
+const filterTransactionsModal = document.getElementById('filter-transactions-modal');
 const editTransactionModal = document.getElementById('edit-transaction-modal');
 
 const addTransactionForm = document.getElementById('add-transaction-form');
-const findTransactionsForm = document.getElementById('find-transactions-form');
+const filterTransactionsForm = document.getElementById('filter-transactions-form');
 const editTransactionForm = document.getElementById('edit-transaction-form');
 
 const headerTopContainer = document.getElementById('header-top');
@@ -25,57 +25,61 @@ const editTransactionButtons = document.getElementsByClassName('edit-transaction
 const deleteTransactionButtons = document.getElementsByClassName('delete-transaction-button');
 
 const addTransactionFormValidator = new transactionValidate.AddTransactionValidator('add-transaction-form');
-const findTransactionsFormValidator = new transactionValidate.FindTransactionsValidator('find-transactions-form');
+const filterTransactionsFormValidator = new transactionValidate.FilterTransactionsValidator('filter-transactions-form');
 const editTransactionFormValidator = new transactionValidate.EditTransactionValidator('edit-transaction-form');
 
 function addAndDisplayTransactions(transactionsJson) {
     for (let transaction of transactionsJson) {
         // creating a row in transactions table for a new transaction
         let row = transactionTableBody.insertRow();
-        row.id = transaction.uuid;
+            row.id = transaction.uuid;
+            
         let dateColumn = row.insertCell(0);
-        dateColumn.innerHTML = dateUtil.getShortDate(transaction.date);
-        dateColumn.className = "date";
+            dateColumn.innerHTML = dateUtil.getShortDate(transaction.date);
+            dateColumn.className = 'date';
+            
         let descriptionColumn = row.insertCell(1);
-        descriptionColumn.innerHTML = transaction.description;
-        descriptionColumn.className = "description";
+            descriptionColumn.innerHTML = transaction.description;
+            descriptionColumn.className = 'description';
+            
         let categoryColumn = row.insertCell(2);
-        categoryColumn.innerHTML = transaction.category;
-        categoryColumn.className = "category";
+            categoryColumn.innerHTML = transaction.category;
+            categoryColumn.className = 'category';
+            
         let amountColumn = row.insertCell(3);
-        amountColumn.innerHTML = transaction.amount;
-        amountColumn.className = "amount";
+            amountColumn.innerHTML = transaction.amount;
+            amountColumn.className = 'amount';
+            
         let typeColumn = row.insertCell(4);
-        typeColumn.innerHTML = transaction.type;
-        typeColumn.className = "type";
+            typeColumn.innerHTML = transaction.type;
+            typeColumn.className = 'type';
 
         let actionsColumn = row.insertCell(5);
 
-        let editButton = document.createElement("button");
+        let editButton = document.createElement('button');
+            editButton.className = 'edit-transaction-button';
 
-        editButton.className = "edit-transaction-button";
+        let editButtonIcon = document.createElement('span');
+            editButtonIcon.className = 'icon';
 
-        let editButtonIcon = document.createElement("span");
-        editButtonIcon.className = "icon";
+        let editIClass = document.createElement('i');
+            editIClass.classList.add('fas', 'fa-edit');
 
-        let editIClass = document.createElement("i");
-        editIClass.classList.add("fas", "fa-edit");
-        editButtonIcon.append(editIClass);
-        editButton.append(editButtonIcon);
+            editButtonIcon.append(editIClass);
+            editButton.append(editButtonIcon);
 
-        let deleteButton = document.createElement("button");
+        let deleteButton = document.createElement('button');
+            deleteButton.className = 'delete-transaction-button';
 
-        deleteButton.className = "delete-transaction-button";
+        let deleteButtonIcon = document.createElement('span');
+            deleteButtonIcon.className = 'icon';
 
-        let deleteButtonIcon = document.createElement("span");
-        deleteButtonIcon.className = "icon";
+        let deleteIClass = document.createElement('i');
+            deleteIClass.classList.add('fas', 'fa-trash-alt');
+            deleteButtonIcon.append(deleteIClass);
+            deleteButton.append(deleteButtonIcon);
 
-        let deleteIClass = document.createElement("i");
-        deleteIClass.classList.add("fas", "fa-trash-alt");
-        deleteButtonIcon.append(deleteIClass);
-        deleteButton.append(deleteButtonIcon);
-
-        actionsColumn.append(editButton, deleteButton);
+            actionsColumn.append(editButton, deleteButton);
     }
     displayUtil.displayElement(transactionContainer);
 }
@@ -101,22 +105,22 @@ addTransactionButton.addEventListener('click', () => {
     displayUtil.displayElement(addTransactionModal);
 });
 
-findTransactionsButton.addEventListener('click', () => {
-    displayUtil.displayElement(findTransactionsModal);
+filterTransactionsButton.addEventListener('click', () => {
+    displayUtil.displayElement(filterTransactionsModal);
 });
 
 //add action listeners to edit buttons
-for(var i = 0; i < editTransactionButtons.length; i++){
+for(let i = 0; i < editTransactionButtons.length; i++){
         let currentButton = editTransactionButtons[i];
         let parentTr = currentButton.parentNode.parentNode;
 
         currentButton.addEventListener('click', () => {
             document.getElementById('edit-id').value = parentTr.id;
-            document.getElementById('edit-description').value = parentTr.querySelector(".description").innerHTML;
-            document.getElementById('edit-date').value = parentTr.querySelector(".date").innerHTML;
-            document.getElementById('edit-type').value = parentTr.querySelector(".type").innerHTML;
-            document.getElementById('edit-category').value = parentTr.querySelector(".category").innerHTML;
-            document.getElementById('edit-amount').value = parentTr.querySelector(".amount").innerHTML;
+            document.getElementById('edit-description').value = parentTr.querySelector('.description').innerHTML;
+            document.getElementById('edit-date').value = parentTr.querySelector('.date').innerHTML;
+            document.getElementById('edit-type').value = parentTr.querySelector('.type').innerHTML;
+            document.getElementById('edit-category').value = parentTr.querySelector('.category').innerHTML;
+            document.getElementById('edit-amount').value = parentTr.querySelector('.amount').innerHTML;
             displayUtil.displayElement(editTransactionModal);
         }
     );
@@ -128,19 +132,12 @@ for(let i = 0; i < deleteTransactionButtons.length; i++){
     let parentTr = currentButton.parentNode.parentNode;
 
     currentButton.addEventListener('click', () => {
-            if (confirm('Are you sure you want to delete transaction: ' + parentTr.querySelector(".description").innerHTML + "?")) {
-                // storing the list to local storage
-
+            if (confirm(`Are you sure you want to delete transaction: ${parentTr.querySelector('.description').innerHTML} + ?`)) {
                 let newTransactions = jsonUtil.deleteTransactionByUuid(transactionsJson, parentTr.id);
                 let jsonString = JSON.stringify(newTransactions);
                 localStorage.setItem('ls-transactions', jsonString);
                 document.getElementById('transactions-table-body').removeChild(parentTr);
-                // alert('Transaction ' + parentTr.querySelector('.description').innerHTML + ' was successfully deleted');
-
-            } else {
-            //    do nothing
-            }
-
+            } 
         }
     );
 }
@@ -148,8 +145,8 @@ for(let i = 0; i < deleteTransactionButtons.length; i++){
 window.addEventListener('click', (event) => {
     if (event.target === addTransactionModal) {
         displayUtil.hideElement(addTransactionModal);
-    } else if (event.target === findTransactionsModal) {
-        displayUtil.hideElement(findTransactionsModal);
+    } else if (event.target === filterTransactionsModal) {
+        displayUtil.hideElement(filterTransactionsModal);
     } else if (event.target === editTransactionModal) {
         displayUtil.hideElement(editTransactionModal);
     }
@@ -176,15 +173,15 @@ addTransactionForm.addEventListener('submit', () => {
     }
 });
 
-findTransactionsForm.addEventListener('submit', () => {
-    if (transactionValidate.FindTransactionsValidator.prototype.isValid) {
+filterTransactionsForm.addEventListener('submit', () => {
+    if (transactionValidate.FilterTransactionsValidator.prototype.isValid) {
         // getting the values from the input form
-        let dateAfter = new Date(document.getElementById('find-date-after').value);
-        let dateBefore = new Date(document.getElementById('find-date-before').value);
-        let type = document.getElementById('find-type').value;
-        let category = document.getElementById('find-category').value;
-        let lessThan = parseFloat(document.getElementById('find-less-than').value);
-        let moreThan = parseFloat(document.getElementById('find-more-than').value);
+        let dateAfter = new Date(document.getElementById('filter-date-after').value);
+        let dateBefore = new Date(document.getElementById('filter-date-before').value);
+        let type = document.getElementById('filter-type').value;
+        let category = document.getElementById('filter-category').value;
+        let lessThan = parseFloat(document.getElementById('filter-less-than').value);
+        let moreThan = parseFloat(document.getElementById('filter-more-than').value);
 
         let filteredTransactions = transactionsJson;
 
@@ -213,7 +210,6 @@ findTransactionsForm.addEventListener('submit', () => {
 });
 
 editTransactionForm.addEventListener('submit', () => {
-    console.log("editTransactionForm.addEventListener is working 1");
     if (transactionValidate.EditTransactionValidator.prototype.isValid) {
         // getting the values from the input form
         let uuid = document.getElementById('edit-id').value;
@@ -222,8 +218,6 @@ editTransactionForm.addEventListener('submit', () => {
         let description = document.getElementById('edit-description').value;
         let category = document.getElementById('edit-category').value;
         let amount = document.getElementById('edit-amount').value;
-
-        console.log("editTransactionForm.addEventListener is working");
 
         // creating a new Transaction object
         let transaction = new Transaction(date, type, description, category, amount);
@@ -236,5 +230,3 @@ editTransactionForm.addEventListener('submit', () => {
         localStorage.setItem('ls-transactions', jsonString);
     }
 });
-
-// TODO: implement edit/delete/find a transaction
