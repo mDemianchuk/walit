620952d428479a36d6d40ef0ffb2aece28b4b073
mdemianchuk <nickdemianchuk@gmail.com>
2019-04-28 03:03:43 -0500

    feat: firebase login and signup

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   entry: {
-    transactions: './controllers/transactions.js',
-    settings: './controllers/settings.js',
-    overview: './controllers/overview.js',
-    login_validate: './utils/validate/login-validate.js',
-    signup_validate: './utils/validate/signup-validate.js',
-    transactions_validate: './utils/validate/transactions-validate.js'
+    transactions: './src/controllers/transactions-controller.js',
+    settings: './src/controllers/settings-controller.js',
+    overview: './src/controllers/overview-controller.js',
+    login: './src/controllers/login-controller.js',
+    login_validate: './src/utils/validate/login-validate.js',
+    signup: './src/controllers/signup-controller.js',
+    signup_validate: './src/utils/validate/signup-validate.js',
+    transactions_validate: './src/utils/validate/transactions-validate.js'
   },
   output: {
     path: __dirname + '/public/scriptBundles',
