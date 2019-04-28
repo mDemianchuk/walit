9ef9ccbf61a26626d262648b3439e73f452bd2ea
mdemianchuk <nickdemianchuk@gmail.com>
2019-04-28 05:18:58 -0500

    feat: user settings

--- src/controllers/settings-controller.js
+++ src/controllers/settings-controller.js
@@ -1,34 +1,73 @@
 const SettingsFormValidator = require('../utils/validate/settings-validate');
-const jsonUtil = require('../utils/json-util');
 
-const settingsForm = document.getElementById('settings-form');
-const settingsFormValidator = new SettingsFormValidator('settings-form');
-
-settingsForm.addEventListener('submit', () => {
-    if (SettingsFormValidator.prototype.isValid) {
-        const limit = parseFloat(document.getElementById('limit').value);
-        const currency = document.getElementById('currency').value;
-        const goal = document.getElementById('goal').value;
-
-        const userSettings = localStorage.getItem('user-settings');
-        let userSettingsJson = JSON.parse(userSettings);
-        if (!jsonUtil.isValidJson(userSettingsJson)) {
-            userSettingsJson = {};
-        }
-
-        if (goal && !isNaN(goal)) {
-            userSettingsJson.goal = goal;
-        }
-
-        if (limit && !isNaN(limit)) {
-            userSettingsJson.limit = limit;
-        }
-
+function updateCurrency(currency, userSettings) {
+    return new Promise(async (resolve, reject) => {
         if (currency) {
-            userSettingsJson.currency = currency;
+            console.log('updating currency');
+            await userSettings.update({
+                currency: currency
+            }).then(() => {
+                resolve(userSettings);
+            });
         }
+        resolve(userSettings);
+    });
+}
 
-        let userSettingsStr = JSON.stringify(userSettingsJson);
-        localStorage.setItem('user-settings', userSettingsStr);
-    }
+function updateGoal(goal, userSettings) {
+    return new Promise(async (resolve, reject) => {
+        if (goal && !isNaN(goal)) {
+            console.log('updating goal');
+            await userSettings.update({
+                goal: goal
+            }).then(() => {
+                resolve(userSettings);
+            });
+        }
+        resolve(userSettings);
+    });
+}
+
+function updateLimit(limit, userSettings) {
+    return new Promise(async (resolve, reject) => {
+        if (limit && !isNaN(limit)) {
+            console.log('updating limit');
+            await userSettings.update({
+                limit: limit
+            }).then(() => {
+                resolve(userSettings);
+            });
+        }
+        resolve(userSettings);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', event => {
+    const settingsForm = document.getElementById('settings-form');
+    const settingsFormValidator = new SettingsFormValidator('settings-form');
+
+    settingsForm.addEventListener('submit', (form) => {
+        form.preventDefault();
+
+        if (SettingsFormValidator.prototype.isValid) {
+            const currency = document.getElementById('currency').value;
+            const goal = parseFloat(document.getElementById('goal').value);
+            const limit = parseFloat(document.getElementById('limit').value);
+            const user = firebase.auth().currentUser;
+            if (user) {
+                const userSettings = firebase.firestore().collection(user.uid).doc('settings');
+
+                updateCurrency(currency, userSettings)
+                    .then(() => updateGoal(goal, userSettings))
+                    .then(() => updateLimit(limit, userSettings))
+                    .then(() => {
+                        alert('User settings are successfully updated');
+                        settingsForm.submit();
+                    });
+
+            } else {
+                alert('You\'re not signed in')
+            }
+        }
+    });
 });
