e15ae8115c9acf933c16d284b387896db4c12c4e
mdemianchuk <nickdemianchuk@gmail.com>
2019-04-28 23:23:59 -0500

    fix: edit transaction

--- src/controllers/transactions-controller.js
+++ src/controllers/transactions-controller.js
@@ -6,6 +6,7 @@ const transactionValidate = require('../utils/validate/transactions-validate');
 
 const transactionContainer = document.getElementById('transactions-container');
 const editTransactionModal = document.getElementById('edit-transaction-modal');
+const buttonContainer = document.getElementById('button-container');
 const addTransactionButton = document.getElementById('add-transaction-button');
 const filterTransactionsButton = document.getElementById('filter-transactions-button');
 
@@ -19,86 +20,91 @@ const editTransactionForm = document.getElementById('edit-transaction-form');
 const headerTopContainer = document.getElementById('header-top');
 const noTransactionsContainer = document.getElementById('no-transactions');
 const signOutButton = document.getElementById('sign-out');
+const loader = document.getElementById('loader');
 
 const addTransactionFormValidator = new transactionValidate.AddTransactionValidator('add-transaction-form');
 const filterTransactionsFormValidator = new transactionValidate.FilterTransactionsValidator('filter-transactions-form');
 const editTransactionFormValidator = new transactionValidate.EditTransactionValidator('edit-transaction-form');
 
-function addAndDisplayTransactions(userId, userTransactions, transaction) {
+function addAndDisplayTransactions(userId, userTransactions, transactionsJson) {
     const transactionTableBody = document.getElementById('transactions-table-body');
 
-    let row = transactionTableBody.insertRow();
-    row.id = transaction.uuid;
+    for (let transaction of transactionsJson) {
 
-    const transactionDate = dateUtil.getShortDate(transaction.date.toDate());
-    let dateColumn = row.insertCell(0);
-    dateColumn.innerHTML = transactionDate;
-    dateColumn.className = 'date';
+        let row = transactionTableBody.insertRow();
+        row.id = transaction.uuid;
 
-    let descriptionColumn = row.insertCell(1);
-    descriptionColumn.innerHTML = transaction.description;
-    descriptionColumn.className = 'description';
+        const transactionDate = dateUtil.getShortDate(transaction.date.toDate());
+        let dateColumn = row.insertCell(0);
+        dateColumn.innerHTML = transactionDate;
+        dateColumn.className = 'date';
 
-    let categoryColumn = row.insertCell(2);
-    categoryColumn.innerHTML = transaction.category;
-    categoryColumn.className = 'category';
+        let descriptionColumn = row.insertCell(1);
+        descriptionColumn.innerHTML = transaction.description;
+        descriptionColumn.className = 'description';
 
-    let amountColumn = row.insertCell(3);
-    amountColumn.innerHTML = transaction.amount;
-    amountColumn.className = 'amount';
+        let categoryColumn = row.insertCell(2);
+        categoryColumn.innerHTML = transaction.category;
+        categoryColumn.className = 'category';
 
-    let typeColumn = row.insertCell(4);
-    typeColumn.innerHTML = transaction.type;
-    typeColumn.className = 'type';
+        let amountColumn = row.insertCell(3);
+        amountColumn.innerHTML = transaction.amount;
+        amountColumn.className = 'amount';
 
-    let actionsColumn = row.insertCell(5);
+        let typeColumn = row.insertCell(4);
+        typeColumn.innerHTML = transaction.type;
+        typeColumn.className = 'type';
 
-    let editButton = document.createElement('button');
-    editButton.className = 'edit-transaction-button';
+        let actionsColumn = row.insertCell(5);
 
-    let editButtonIcon = document.createElement('span');
-    editButtonIcon.className = 'icon';
+        let editButton = document.createElement('button');
+        editButton.className = 'edit-transaction-button';
 
-    let editIClass = document.createElement('i');
-    editIClass.classList.add('fas', 'fa-edit');
+        let editButtonIcon = document.createElement('span');
+        editButtonIcon.className = 'icon';
 
-    editButtonIcon.append(editIClass);
-    editButton.append(editButtonIcon);
+        let editIClass = document.createElement('i');
+        editIClass.classList.add('fas', 'fa-edit');
 
-    editButton.addEventListener('click', () => {
-            document.getElementById('edit-id').value = transaction.uuid;
-            document.getElementById('edit-description').value = transaction.description;
-            document.getElementById('edit-date').value = transactionDate;
-            document.getElementById('edit-type').value = transaction.type;
-            document.getElementById('edit-category').value = transaction.category;
-            document.getElementById('edit-amount').value = transaction.amount;
-            displayUtil.displayElement(editTransactionModal);
-        }
-    );
+        editButtonIcon.append(editIClass);
+        editButton.append(editButtonIcon);
 
-    let deleteButton = document.createElement('button');
-    deleteButton.className = 'delete-transaction-button';
-
-    let deleteButtonIcon = document.createElement('span');
-    deleteButtonIcon.className = 'icon';
-
-    let deleteIClass = document.createElement('i');
-    deleteIClass.classList.add('fas', 'fa-trash-alt');
-    deleteButtonIcon.append(deleteIClass);
-    deleteButton.append(deleteButtonIcon);
-
-    deleteButton.addEventListener('click', () => {
-            if (confirm(`Are you sure you want to delete transaction: ${transaction.description} ?`)) {
-                userTransactions.doc(`${userId}/transactions/${transaction.uuid}`)
-                    .delete()
-                    .then(() => {
-                        alert('deleted')
-                    })
+        editButton.addEventListener('click', () => {
+                document.getElementById('edit-id').value = transaction.uuid;
+                document.getElementById('edit-description').value = transaction.description;
+                document.getElementById('edit-date').value = transactionDate;
+                document.getElementById('edit-type').value = transaction.type;
+                document.getElementById('edit-category').value = transaction.category;
+                document.getElementById('edit-amount').value = transaction.amount;
+                displayUtil.displayElement(editTransactionModal);
             }
-        }
-    );
+        );
 
-    actionsColumn.append(editButton, deleteButton);
+        let deleteButton = document.createElement('button');
+        deleteButton.className = 'delete-transaction-button';
+
+        let deleteButtonIcon = document.createElement('span');
+        deleteButtonIcon.className = 'icon';
+
+        let deleteIClass = document.createElement('i');
+        deleteIClass.classList.add('fas', 'fa-trash-alt');
+        deleteButtonIcon.append(deleteIClass);
+        deleteButton.append(deleteButtonIcon);
+
+        deleteButton.addEventListener('click', () => {
+                if (confirm(`Are you sure you want to delete transaction: ${transaction.description} ?`)) {
+                    userTransactions.doc(`${userId}/transactions/${transaction.uuid}`)
+                        .delete()
+                        .then(() => {
+                            window.location.replace('/transactions');
+                        });
+                }
+            }
+        );
+
+        actionsColumn.append(editButton, deleteButton);
+    }
+    displayUtil.displayElement(transactionContainer);
 }
 
 function clearTable() {
@@ -108,44 +114,30 @@ function clearTable() {
     transactionTableBody.parentNode.replaceChild(emptyTableBody, transactionTableBody);
 }
 
-function loadThePage(userId, userTransactions) {
-
-    displayUtil.hideElement(noTransactionsContainer);
-    displayUtil.displayElement(headerTopContainer);
-
-    const transactions = userTransactions.doc(userId)
-        .collection('transactions')
-        .orderBy('date', 'desc')
-        .get()
-        .then(snap => {
-            snap.forEach((doc) => {
-                addAndDisplayTransactions(userId, userTransactions, doc.data())
-            });
-            displayUtil.displayElement(transactionContainer);
-        });
-
-
-    // adding event listeners
-    addTransactionButton.addEventListener('click', () => {
-        displayUtil.displayElement(addTransactionModal);
-    });
-
-    filterTransactionsButton.addEventListener('click', () => {
-        displayUtil.displayElement(filterTransactionsModal);
-    });
+async function loadThePage(userId, userTransactions) {
 
     signOutButton.addEventListener('click', () => {
         firebase.auth().signOut();
     });
 
-    window.addEventListener('click', (event) => {
-        if (event.target === addTransactionModal) {
-            displayUtil.hideElement(addTransactionModal);
-        } else if (event.target === filterTransactionsModal) {
-            displayUtil.hideElement(filterTransactionsModal);
-        } else if (event.target === editTransactionModal) {
-            displayUtil.hideElement(editTransactionModal);
-        }
+    let transactionsJson = [];
+
+    await userTransactions.doc(userId)
+        .collection('transactions')
+        .orderBy('date', 'desc')
+        .get()
+        .then(snap => {
+            snap.forEach((doc) => {
+                transactionsJson.push(doc.data());
+            });
+        });
+
+    displayUtil.hideElement(loader);
+    displayUtil.displayElement(buttonContainer);
+
+    // adding event listeners
+    addTransactionButton.addEventListener('click', () => {
+        displayUtil.displayElement(addTransactionModal);
     });
 
     addTransactionForm.addEventListener('submit', (form) => {
@@ -176,84 +168,104 @@ function loadThePage(userId, userTransactions) {
         }
     });
 
-
-    filterTransactionsForm.addEventListener('submit', (form) => {
-        form.preventDefault();
-
-        if (transactionValidate.FilterTransactionsValidator.prototype.isValid) {
-            // getting the values from the input form
-            let dateAfter = new Date(document.getElementById('filter-date-after').value);
-            let dateBefore = new Date(document.getElementById('filter-date-before').value);
-            let type = document.getElementById('filter-type').value;
-            let category = document.getElementById('filter-category').value;
-            let lessThan = parseFloat(document.getElementById('filter-less-than').value);
-            let moreThan = parseFloat(document.getElementById('filter-more-than').value);
-
-            let filteredTransactions = [];
-
-            userTransactions.doc(userId)
-                .collection('transactions')
-                .get()
-                .then(snap => {
-                    snap.forEach(doc => filteredTransactions.push(doc.data()))
-                })
-                .then(() => {
-                    if (!isNaN(dateAfter.getDate())) {
-                        filteredTransactions = jsonUtil.getTransactionsAfterDate(filteredTransactions, dateAfter);
-                    }
-                    if (!isNaN(dateBefore.getDate())) {
-                        filteredTransactions = jsonUtil.getTransactionsBeforeDate(filteredTransactions, dateBefore);
-                    }
-                    if (type) {
-                        filteredTransactions = jsonUtil.getTransactionsByType(filteredTransactions, type);
-                    }
-                    if (category) {
-                        filteredTransactions = jsonUtil.getTransactionsByCategory(filteredTransactions, category);
-                    }
-                    if (!isNaN(lessThan)) {
-                        filteredTransactions = jsonUtil.getTransactionsLessThan(filteredTransactions, lessThan);
-                    }
-                    if (!isNaN(moreThan)) {
-                        filteredTransactions = jsonUtil.getTransactionsMoreThan(filteredTransactions, moreThan);
-                    }
-                    if (filteredTransactions) {
-                        clearTable();
-                        filteredTransactions.forEach((transaction) => {
-                            addAndDisplayTransactions(userId, userTransactions, transaction)
-                        });
-                    }
-                    filterTransactionsModal.click();
-                })
-
+    window.addEventListener('click', (event) => {
+        if (event.target === addTransactionModal) {
+            displayUtil.hideElement(addTransactionModal);
+        } else if (event.target === filterTransactionsModal) {
+            displayUtil.hideElement(filterTransactionsModal);
+        } else if (event.target === editTransactionModal) {
+            displayUtil.hideElement(editTransactionModal);
         }
     });
 
-    editTransactionForm.addEventListener('submit', (form) => {
-        form.preventDefault();
+    if (transactionsJson.length === 0) {
+        displayUtil.displayElement(noTransactionsContainer);
+        displayUtil.hideElement(filterTransactionsButton);
+    } else {
+        displayUtil.displayElement(headerTopContainer);
+        addAndDisplayTransactions(userId, userTransactions, transactionsJson);
 
-        if (transactionValidate.EditTransactionValidator.prototype.isValid) {
-            // getting the values from the input form
-            let uuid = document.getElementById('edit-id').value;
-            let date = document.getElementById('edit-date').value;
-            let type = document.getElementById('edit-type').value;
-            let description = document.getElementById('edit-description').value;
-            let category = document.getElementById('edit-category').value;
-            let amount = parseFloat(document.getElementById('edit-amount').value);
+        filterTransactionsButton.addEventListener('click', () => {
+            displayUtil.displayElement(filterTransactionsModal);
+        });
 
-            const newTransactionRef = userTransactions.doc(`${userId}/transactions/${uuid}`);
+        filterTransactionsForm.addEventListener('submit', (form) => {
+            form.preventDefault();
 
-            const newTransaction = {
-                description: description,
-                category: category,
-                date: date,
-                amount: amount,
-                type: type,
-            };
+            if (transactionValidate.FilterTransactionsValidator.prototype.isValid) {
+                // getting the values from the input form
+                let dateAfter = new Date(document.getElementById('filter-date-after').value);
+                let dateBefore = new Date(document.getElementById('filter-date-before').value);
+                let type = document.getElementById('filter-type').value;
+                let category = document.getElementById('filter-category').value;
+                let lessThan = parseFloat(document.getElementById('filter-less-than').value);
+                let moreThan = parseFloat(document.getElementById('filter-more-than').value);
 
-            newTransactionRef.update(newTransaction)
-                .then(() => editTransactionForm.submit())
-        }
-    });
+                let filteredTransactions = [];
+
+                userTransactions.doc(userId)
+                    .collection('transactions')
+                    .get()
+                    .then(snap => {
+                        snap.forEach(doc => filteredTransactions.push(doc.data()))
+                    })
+                    .then(async () => {
+                        if (!isNaN(dateAfter.getDate())) {
+                            filteredTransactions = jsonUtil.getTransactionsAfterDate(filteredTransactions, dateAfter);
+                        }
+                        if (!isNaN(dateBefore.getDate())) {
+                            filteredTransactions = jsonUtil.getTransactionsBeforeDate(filteredTransactions, dateBefore);
+                        }
+                        if (type) {
+                            filteredTransactions = jsonUtil.getTransactionsByType(filteredTransactions, type);
+                        }
+                        if (category) {
+                            filteredTransactions = jsonUtil.getTransactionsByCategory(filteredTransactions, category);
+                        }
+                        if (!isNaN(lessThan)) {
+                            filteredTransactions = jsonUtil.getTransactionsLessThan(filteredTransactions, lessThan);
+                        }
+                        if (!isNaN(moreThan)) {
+                            filteredTransactions = jsonUtil.getTransactionsMoreThan(filteredTransactions, moreThan);
+                        }
+                        if (filteredTransactions.length > 0) {
+                            clearTable();
+                            addAndDisplayTransactions(userId, userTransactions, filteredTransactions);
+                            filterTransactionsModal.click();
+                        } else {
+                            alert('No transactions for given filters');
+                        }
+                    })
+            }
+        });
+
+        editTransactionForm.addEventListener('submit', (form) => {
+            form.preventDefault();
+
+            if (transactionValidate.EditTransactionValidator.prototype.isValid) {
+                // getting the values from the input form
+                let uuid = document.getElementById('edit-id').value;
+                let date = new Date(document.getElementById('edit-date').value);
+                let type = document.getElementById('edit-type').value;
+                let description = document.getElementById('edit-description').value;
+                let category = document.getElementById('edit-category').value;
+                let amount = parseFloat(document.getElementById('edit-amount').value);
+
+                const newTransactionRef = userTransactions.doc(`${userId}/transactions/${uuid}`);
+
+                const newTransaction = {
+                    description: description,
+                    category: category,
+                    date: date,
+                    amount: amount,
+                    type: type,
+                };
+
+                newTransactionRef.update(newTransaction)
+                    .then(() => editTransactionForm.submit())
+            }
+        });
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
