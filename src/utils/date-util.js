4f92490869dc4b7be4d8af9e43db6a0b69e91acb
mdemianchuk <nickdemianchuk@gmail.com>
2019-05-02 22:12:29 -0500

    fix: operations with date objects

--- src/utils/date-util.js
+++ src/utils/date-util.js
@@ -1,16 +1,16 @@
-function getFirstDayInMonth(numberOfMonths) {
+function getFirstDayInMonth(numberOfMonthsToSubtract) {
     let currentDate = new Date();
-    return new Date(currentDate.getFullYear(), currentDate.getMonth() - numberOfMonths, 1);
+    return new Date(currentDate.getFullYear(), currentDate.getMonth() - numberOfMonthsToSubtract, 1);
 }
 
-function getLastDayInMonth(numberOfMonths) {
-    let currentDate = new Date();
-    return new Date(currentDate.getFullYear(), currentDate.getMonth() + 1 - numberOfMonths, 0);
+function getLastDayInMonth(numberOfMonthsToSubtract) {
+    let lastDayInMonth = getFirstDayInMonth(numberOfMonthsToSubtract - 1);
+    return new Date(lastDayInMonth.getTime() - 1);
 }
 
 function getShortDate(date) {
     let month = toTwoDigit(date.getMonth() + 1);
-    let day = toTwoDigit(date.getDate() + 1);
+    let day = toTwoDigit(date.getDate());
     let year = date.getFullYear();
     return `${month}/${day}/${year}`;
 
